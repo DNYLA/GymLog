a08b663624754fbdb029a46e7cb7d9e1eceab3d0
DanA <22224922+DNYLA@users.noreply.github.com>
2021-10-24 17:19:44 +0100

    React Native App

--- App.tsx
+++ App.tsx
@@ -1,12 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [day, setDay] = useState('monday');
+  const workout = {
+    monday: {
+      name: 'Heavy Lower',
+      squat: {
+        sets: 3,
+        reps: 6,
+      },
+      deadlift: {
+        sets: 2,
+        reps: 6,
+      },
+    },
+    tuesday: {
+      name: 'Heavy Upper',
+      benchPress: {
+        name: 'Bench Press',
+        sets: 3,
+        reps: 6,
+      },
+    },
+  };
+  const name = 'dan';
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>GymLog App {name}</Text>
+      <Text>Here Is Your Workout Routine</Text>
+      <Text>{workout.monday.squat.sets}</Text>
+      <TextInput style={styles.inputBox} defaultValue="Enter Your Day" />
       <StatusBar style="auto" />
+      <Button
+        onPress={() => {
+          setDay('tuesday');
+        }}
+        title="Tuesday"
+      />
     </View>
   );
 }
@@ -17,5 +49,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  inputBox: {
+    height: 40,
+    width: '90%',
+    borderColor: 'gray',
+    borderWidth: 1,
   },
 });
