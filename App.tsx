c7c56715e47b9ffc09b0a3c8df6ba22cf9b02048
DanA <22224922+DNYLA@users.noreply.github.com>
2021-10-24 22:07:51 +0100

    Navigation Added

--- App.tsx
+++ App.tsx
@@ -1,26 +1,51 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
 import Home from './src/Screens/Home';
+import Plan from './src/Screens/Plans';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import {
+  faCoffee,
+  faCog,
+  faExclamation,
+  faExclamationCircle,
+  faExclamationTriangle,
+  faHome,
+} from '@fortawesome/free-solid-svg-icons';
 
 export default function App() {
+  const Tab = createBottomTabNavigator();
+
   return (
-    <View style={styles.container}>
-      <Home />
-    </View>
+    // <NavigationContainer>
+    //   {' '}
+    //   <View style={styles.container}>
+    //     <Home />
+    //   </View>
+    // </NavigationContainer>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let icon = faExclamationCircle;
+
+            if (route.name === 'Home') {
+              icon = focused ? faHome : faHome;
+            } else if (route.name === 'Settings') {
+              icon = focused ? faCog : faCog;
+            }
+
+            // You can return any component that you like here!
+            return <FontAwesomeIcon icon={icon} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Routine" component={Home} />
+        <Tab.Screen name="Settings" component={Plan} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputBox: {
-    height: 40,
-    width: '90%',
-    borderColor: 'gray',
-    borderWidth: 1,
-  },
-});
---
+++ src/Screens/Plans.tsx
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Plan = () => {
+  return (
+    <View style={styles.container}>
+      <Text>This is a Plan</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputBox: {
+    height: 40,
+    width: '90%',
+    borderColor: 'gray',
+    borderWidth: 1,
+  },
+});
+
+export default Plan;
